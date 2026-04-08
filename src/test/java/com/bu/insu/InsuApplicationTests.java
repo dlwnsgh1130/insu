d7b90d4af6a612a1e5afc8543de3f059bddd1473
dlwnsgh1130 <dlwnsgh1130@bu.ac.kr>
2026-04-08 21:00:56 +0900

    첫번쨰

---
+++ src/test/java/com/bu/insu/InsuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bu.insu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
